5b1e41b858bffba307d70fb47922681b178c90b7
subhash birajdar <subhashchandrabirajdar98@gmail.com>
2024-08-27 07:08:44 +0530

    initial commit

---
+++ src/main/java/com/apifiltering/RestAPIFiltering/RestApiFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.apifiltering.RestAPIFiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiFilteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apifiltering/RestAPIFiltering/RestApiFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apifiltering.RestAPIFiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiFilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
